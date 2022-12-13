a952585025e63055f2b2a8ebdbbe5aa7f78b085c
v8-fly <98163507+v8-fly@users.noreply.github.com>
2022-12-13 08:39:42 +0530

    normal-get-pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,24 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+import { useEffect, useState } from "react"
+
+const PokemonList = ({ pokemon }) => {
+  console.log("PokemonList")
+  return (
+    <ul>
+      {pokemon.map((d) => (
+        <li key={d.id}>{d.name}</li>
+      ))}
+    </ul>
+  )
 }
+
+const App = () => {
+  const [pokemon, setPokemon] = useState([])
+  useEffect(() => {
+    fetch("../public/pokemon.json")
+      .then((res) => res.json())
+      .then((data) => setPokemon(data))
+  }, [])
+  return <PokemonList pokemon={pokemon}></PokemonList>
+}
+
+export default App
