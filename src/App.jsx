77d2decbfb6241719d6b635cdeecd95616ae1b0f
v8-fly <98163507+v8-fly@users.noreply.github.com>
2022-12-15 00:30:05 +0530

    Using UseReducer for states

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { PokemonProvider, usePokemonContext } from "./store"
 
 const SearchPokemon = () => {
-  const { search, setSearch } = usePokemonContext()
+  const { search, setPokemonSearch } = usePokemonContext()
   return (
     <input
       type="text"
       className="mt-3 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-800 focus:ring-indigo-800 sm:text-lg p-2"
       placeholder="Search Pokemon"
       value={search}
-      onChange={(e) => setSearch(e.target.value)}
+      onChange={(e) => setPokemonSearch(e.target.value)}
     />
   )
 }
--- src/store.jsx
+++ src/store.jsx
@@ -1,19 +1,61 @@
-import { createContext, useState, useEffect, useContext } from "react"
+import {
+  createContext,
+  useEffect,
+  useContext,
+  useReducer,
+  useCallback,
+} from "react"
 
 export const PokemonContext = createContext()
 PokemonContext.displayName = "PokemonContext"
 export const usePokemonContext = () => useContext(PokemonContext)
 
 const usePokemonData = () => {
-  const [pokemon, setPokemon] = useState([])
-  const [search, setSearch] = useState("")
+  const [{ pokemon, search }, dispatch] = useReducer(
+    (state, action) => {
+      switch (action.type) {
+        case "setSearch":
+          return { ...state, search: action.payload }
+        case "setPokemon":
+          return { ...state, pokemon: action.payload }
+
+        default:
+          break
+      }
+    },
+    {
+      pokemon: [],
+      search: "",
+    }
+  )
+
+  // const [pokemon, setPokemon] = useState([])
+  // const [search, setSearch] = useState("")
+
+  const setPokemonSearch = useCallback((searchVal) => {
+    dispatch({
+      type: "setSearch",
+      payload: searchVal,
+    })
+  }, [])
+
   useEffect(() => {
     fetch("../public/pokemon.json")
       .then((res) => res.json())
-      .then((data) => setPokemon(data))
+      .then((data) =>
+        dispatch({
+          type: "setPokemon",
+          payload: data,
+        })
+      )
   }, [])
 
-  return { pokemon, search, setSearch }
+  const filteredPokemon = pokemon.filter((d) => {
+    console.log("filteredPokemon", d.name)
+    return d.name.toLowerCase().includes(search)
+  })
+
+  return { pokemon: filteredPokemon, search, setPokemonSearch }
 }
 export const PokemonProvider = ({ children }) => (
   <PokemonContext.Provider value={usePokemonData()}>
